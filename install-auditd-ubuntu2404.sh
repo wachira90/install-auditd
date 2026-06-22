d77c19ee44ca702b2ce1064a05e1cf9f392b877f
Wachira Duangdee <15179736+wachira90@users.noreply.github.com>
2026-06-23 03:01:35 +0700

    Add script to install and configure auditd on Ubuntu 24.04

---
+++ install-auditd-ubuntu2404.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+# install-auditd-ubuntu2404.sh
+sudo apt update
+sudo apt install auditd audispd-plugins -y
+sudo filebeat modules enable auditd
+sudo cp auditd.conf /etc/audit/auditd.conf
+sudo cp custom.rules /etc/audit/rules.d/custom.rules
+sleep 2
+sudo augenrules --load
+sudo systemctl enable auditd
+sudo systemctl restart auditd
